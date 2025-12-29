33ae1b97676e62a6acf0eea1262d33ce23534ca4
Abhishek Sharma <abhisheksharmaazpm2@gmail.com>
2025-12-30 00:19:06 +0530

    .

--- src/API/auth.api.js
+++ src/API/auth.api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getToken = () => localStorage.getItem("accessToken")
 
 const API = axios.create({
-    baseURL: "http://localhost:4000/api/v1/auth" /*"https://lms-67ch.onrender.com/api/v1/auth"*/
+    baseURL: /*"http://localhost:4000/api/v1/auth"*/ "https://lms-67ch.onrender.com/api/v1/auth"
 })
 
 API.interceptors.request.use((config) => {
--- src/API/module.api.js
+++ src/API/module.api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getToken = () => localStorage.getItem("accessToken")
 
 const API = axios.create({
-    baseURL: "http://localhost:4000/api/v1/modules" /*"https://lms-67ch.onrender.com/api/v1/modules"*/
+    baseURL: /*"http://localhost:4000/api/v1/modules"*/ "https://lms-67ch.onrender.com/api/v1/modules"
 })
 API.interceptors.request.use((config) => {
     const token = getToken()
