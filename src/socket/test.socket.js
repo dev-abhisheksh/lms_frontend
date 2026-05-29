8364ba8068d135d951f7cb5700b2b0685d6f98b5
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-05-29 23:00:05 +0530

    fix: streamline test socket connection and event handling

--- src/socket/test.socket.js
+++ src/socket/test.socket.js
@@ -1,19 +1,17 @@
 import socket from "./index";
 
-export const connectTestSocket = (courseIds, { onPublished }) => {
-  if (!socket.connected) {
-    socket.connect();
-  }
+export const connectTestSocket = (courseId, { onPublished }) => {
+  socket.connect();
 
-  socket.emit("join-courses", courseIds);
+  socket.emit("join-courses", [courseId]);
 
-  // Listen for newly published tests
   socket.on("test:published", (data) => {
-    console.log("New Test published:", data);
-    if (onPublished) onPublished(data);
-  });
-};
+    console.log("New test published:", data)
+    if (onPublished) onPublished(data)
+  })
+}
 
 export const disconnectTestSocket = () => {
   socket.off("test:published");
+  socket.disconnect();
 };
